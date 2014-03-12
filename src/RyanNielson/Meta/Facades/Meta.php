f9125ac5e3b86f5b039e733b5ef7269cec7c445a
Ryan Nielson <ryan.nielson@gmail.com>
2014-03-12 15:28:16 +0000

    Added the Meta facade

---
+++ src/RyanNielson/Meta/Facades/Meta.php
@@ -0,0 +1,14 @@
+<?php namespace RyanNielson\Meta\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Meta extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'meta'; }
+
+}
